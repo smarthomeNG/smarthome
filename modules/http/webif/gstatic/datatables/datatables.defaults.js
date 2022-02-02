e42e5f02a844c434cd5421799538d2172a345c09
Onkel Andy <onkelandy@hotmail.com>
2022-02-02 04:09:57 +0100

    datatables: fix table width in tabs

--- modules/http/webif/gstatic/datatables/datatables.defaults.js
+++ modules/http/webif/gstatic/datatables/datatables.defaults.js
@@ -24,23 +24,28 @@ $(window).bind('datatables_defaults', function() {
 		{
 			// Set datatable useful defaults
 			$.extend( $.fn.dataTable.defaults, {
-						 lengthMenu: [ [10, 25, 50, -1], [10, 25, 50, "All"] ], // pagination menu
-						 pageLength: -1, // default to "all"
-						 pagingType: "full_numbers", // include first and last in pagination menu
-						 colReorder: true, // enable colomn reorder by drag and drop
-						 fixedHeader: {header: true, // header will always be visible on top of page when scrolling
-							 						 headerOffset: $('#webif-navbar').outerHeight() + $('#webif-tabs').outerHeight()},
-						 responsive: true, // enable responsive extension.
-						 "fnDrawCallback": function(oSettings) { // hide pagination if not needed
-							 if (oSettings._iDisplayLength > oSettings.fnRecordsDisplay() || oSettings._iDisplayLength == -1) {
-									 $(oSettings.nTableWrapper).find('.dataTables_paginate').hide();
-							 } else {
-										$(oSettings.nTableWrapper).find('.dataTables_paginate').show();
-							 }
-						 }
+				lengthMenu: [ [10, 25, 50, -1], [10, 25, 50, "All"] ], // pagination menu
+				pageLength: -1, // default to "all"
+				pagingType: "full_numbers", // include first and last in pagination menu
+				colReorder: true, // enable colomn reorder by drag and drop
+				fixedHeader: {header: true, // header will always be visible on top of page when scrolling
+				 						 headerOffset: $('#webif-navbar').outerHeight() + $('#webif-tabs').outerHeight()},
+				responsive: true, // enable responsive extension.
+				autoWidth: false,
+				"fnDrawCallback": function(oSettings) { // hide pagination if not needed
+					if (oSettings._iDisplayLength > oSettings.fnRecordsDisplay() || oSettings._iDisplayLength == -1) {
+						 $(oSettings.nTableWrapper).find('.dataTables_paginate').hide();
+					} else {
+							$(oSettings.nTableWrapper).find('.dataTables_paginate').show();
+					}
+				}
 			});
 			// Set date format for correct sorting of columns containing date strings
 			$.fn.dataTable.moment('DD.MM.YYYY HH:mm:ss');
+			$('a[data-toggle="tab"]').on('shown.bs.tab', function(e){
+				$.fn.dataTable.tables({ visible: true, api: true }).columns.adjust().draw();
+				$.fn.dataTable.tables({ visible: true, api: true }).responsive.recalc();
+			});
 		}
 	catch (e)
 		{
