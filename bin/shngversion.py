de3c260e7d308d93d2ec5128f1d972927136b72f
msinn <msinn@users.noreply.github.com>
2022-05-02 13:16:59 +0200

    Set repo version to post 1.9.2 release -> 1.9.2.1

--- bin/shngversion.py
+++ bin/shngversion.py
@@ -82,7 +82,7 @@ from lib.utils import Version
 # Update auf 1.9.1.2  wg. zusätzlicher Log Level"
 
 # Update auf 1.9.2    wg. Release
-# Update auf 1.9.2.1  wg. Kennzeichnung des Stands als "nach dem v1.9.2 Release"
+# Update auf 1.9.2.1  wg. Kennzeichnung des Repo Stands als "nach dem v1.9.2 Release"
 
 shNG_version = '1.9.2.1'
 shNG_branch = 'develop'
