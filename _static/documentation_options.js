d68a622e172b33c20d2a6169a55afc25fcfe0a35
msinn <msinn@users.noreply.github.com>
2022-05-14 09:42:59 +0000

    Deploying to release_doc from @ smarthomeNG/smarthome@75ff8646606c1d37186de2de96cba73ca95ee1c1 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v1.9.2 (Stand 13. May 2022)',
+    VERSION: 'v1.9.2 (Stand 14. May 2022)',
     LANGUAGE: 'de',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
