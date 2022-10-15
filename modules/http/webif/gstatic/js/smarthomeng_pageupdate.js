7da839d915bdb4b48ffe17447c80970399c49c23
Onkel Andy <onkelandy@hotmail.com>
2022-10-16 01:19:16 +0200

    http module: implement update_blocked to adjust checkbox for "auto refresh" plugin page

--- modules/http/webif/gstatic/js/smarthomeng_pageupdate.js
+++ modules/http/webif/gstatic/js/smarthomeng_pageupdate.js
@@ -2,7 +2,7 @@
  * enables or disables the auto refresh checkbox based on form input for interval
 */
 function set_update_active() {
-  if(document.getElementById("update_interval").value.length == 0 || document.getElementById("update_interval").value == 0)
+  if(document.getElementById("update_interval").value.length == 0 || document.getElementById("update_interval").value == 0 || window.update_blocked == true)
   {
     $('input[name=update_active]').attr('disabled','disabled');
     document.getElementById("update_active").checked = false;
@@ -35,8 +35,8 @@ function set_update_interval() {
  * initialize form values correctly and update active checkbox based on interval value (disabled on 0)
 */
 $(window).on('load', function (e) {
-  document.getElementById("update_interval").value = window.update_interval / 1000;
   document.getElementById("update_active").checked = window.update_active;
+  document.getElementById("update_interval").value = window.update_interval / 1000;
   set_update_active();
   let interval_input = document.getElementById("update_interval");
   interval_input.addEventListener('input', event => {
