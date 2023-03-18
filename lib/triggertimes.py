c653f0e2d71ca572ce403a4dbc0a8421704c5655
msinn <msinn@users.noreply.github.com>
2023-03-18 11:35:10 +0100

    lib.triggertimes: Fixed error message for Python 3.7

--- lib/triggertimes.py
+++ lib/triggertimes.py
@@ -923,7 +923,7 @@ class Skytime(TriggerTime):
                                     eventtime = None
                                 if eventtime is None:
                                     eventtime = get_invalid_time()
-                                    logger.error(f'skyevent {self.event} could not be calculated, setting to invalid {eventtime=} and try to continue')
+                                    logger.error(f"skyevent {self.event} could not be calculated, setting to invalid eventtime '{eventtime}" and try to continue")
                                 # time in next_time will be in utctime. So we need to adjust it
                                 if eventtime.tzinfo == tzutc():
                                     eventtime = eventtime.astimezone(Skytime.sh.shtime.tzinfo())
