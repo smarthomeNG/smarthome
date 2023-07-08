0eff5042c95fc23341ead0f45a4a1542b684ddd0
Onkel Andy <onkelandy@hotmail.com>
2023-07-08 23:58:56 +0200

    http module: introduce set_cookie and get_cookie functions that can be used in plugins for storing values

--- modules/http/webif/gstatic/js/smarthomeng_pageupdate.js
+++ modules/http/webif/gstatic/js/smarthomeng_pageupdate.js
@@ -1,3 +1,40 @@
+function getCookie(cname) {
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(';');
+  for(let i = 0; i <ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      let result = c.substring(name.length, c.length);
+      console.log("Reading cookie " + cname + " with value " + result);
+      return result;
+    }
+  }
+  return "";
+}
+
+function setCookie(cname, cvalue, exdays, path) {
+  if (exdays === 0)
+  {
+    let expires = "expires=Thu, 01 Jan 1970 00:00:00 UTC";
+
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/plugin/" + path;
+    console.log("Deleting cookie " + cname + " for plugin " + path);
+  }
+  else
+  {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    let expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/plugin/" + path;
+    console.log("Setting cookie " + cname + " for plugin " + path + " to " + cvalue + ", all cookies " + document.cookie);
+  }
+
+}
+/* setCookie('update_interval', '', 0, '');*/
 /**
  * enables or disables the auto refresh checkbox based on form input for interval
 */
