822b0d588d5dbce534cb57eec9a9dee5e6340b62
Onkel Andy <onkelandy@hotmail.com>
2024-12-19 15:19:46 +0100

    build docu: fallback for locale

--- doc/user/source/conf.py
+++ doc/user/source/conf.py
@@ -20,8 +20,10 @@ import plugins as pluginsversion
 
 import datetime
 import locale
-locale.setlocale(locale.LC_TIME, 'de_DE.utf8')
-#locale.setlocale(locale.LC_ALL, 'C')
+try:
+    locale.setlocale(locale.LC_TIME, 'de_DE.utf8')
+except Exception:
+    locale.setlocale(locale.LC_ALL, 'C')
 now = datetime.datetime.now()
 import calendar
 
