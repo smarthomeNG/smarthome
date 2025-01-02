a2b15ed4c287e5f16f6cada1b24b14be53829215
Morg42 <43153739+Morg42@users.noreply.github.com>
2025-01-02 19:22:23 +0100

    sdp: fix stupid c&p typo

--- lib/model/smartdeviceplugin.py
+++ lib/model/smartdeviceplugin.py
@@ -1,4 +1,4 @@
-http://microstar.ir/download/SinaProg.zip#!/usr/bin/env python3
+#!/usr/bin/env python3
 # vim: set encoding=utf-8 tabstop=4 softtabstop=4 shiftwidth=4 expandtab
 #########################################################################
 #  Copyright 2020-      Sebastian Helms           Morg @ knx-user-forum
